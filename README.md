9a47d0267b5dcbb0003b09f4f73258605e7b31c2
Soeren Grunewald <soeren.grunewald@gmx.net>
2020-09-29 19:38:47 +0200

    Initial import
    Signed-off-by: Soeren Grunewald <soeren.grunewald@gmx.net>

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# gamepad-fix
-Send initial command to make gamepad work as expected
+# Gamepad Fix
+
+Send an initial command to make gamepad work as expected.
+
+This is basically a rewrite of dnmodder fixcontroller python script.
+https://gist.github.com/dnmodder/de2df973323b7c6acf45f40dc66e8db3
+
---
+++ main.c
@@ -0,0 +1,67 @@
+/**
+ * SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#include <libusb.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+int main(int argc, char *argv[])
+{
+	unsigned char cmd[1] = { 0x14 };
+	struct libusb_device_descriptor d;
+	libusb_device_handle *h;
+	libusb_context *c = NULL;
+	int r;
+
+	/* we need to be able to send control transfer messages
+	 * maybe we could find a better way but for now this will
+	 * do
+	 */
+	if (geteuid() != 0) {
+		fprintf(stderr, "You need to run as root or use sudo");
+		return 1;
+	}
+
+	r = libusb_init(&c);
+	if (r) {
+		fprintf(stderr, "Unable to initialize libusb: %s\n", libusb_strerror(r));
+		return 1;
+	}
+
+	h = libusb_open_device_with_vid_pid(c, 0x045e, 0x028e);
+	if (h == NULL) {
+		fprintf(stderr, "Unable to open usb device\n");
+		goto cleanup;
+	}
+
+	r = libusb_get_device_descriptor(libusb_get_device(h), &d);
+	if (r != 0) {
+		fprintf(stderr, "Unable to query device descriptor: %s\n", libusb_strerror(r));
+		goto cleanup;
+	}
+
+	if (d.iManufacturer != 1 && d.iProduct != 2) {
+		fprintf(stderr, "Not a Shanwan controller\n");
+		goto cleanup;
+	}
+
+	/*
+	 * see https://gist.github.com/dnmodder/de2df973323b7c6acf45f40dc66e8db3
+	 */
+	r = libusb_control_transfer(h, 0xc1, 0x01, 0x0100, 0x00, cmd, sizeof(cmd), 0);
+	if (r < 0) {
+		fprintf(stderr, "Control transfer failed: %s\n", libusb_strerror(r));
+		goto cleanup;
+	}
+
+cleanup:
+	if (h)
+		libusb_close(h);
+
+	libusb_exit(c);
+	return 0;
+}
